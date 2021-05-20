ba5a74300c2d61f06a804bc02ff660e21fe997b4
Monish-Trivedi <monish.trivedi@krishtechnolabs.com>
2021-05-20 11:00:31 +0530

    HelloWorld module in magento2

---
+++ App/Router/NoRouteHandler.php
@@ -0,0 +1,13 @@
+<?php
+namespace Monish\HelloWorld\App\Router;
+class NoRouteHandler implements \Magento\Framework\App\Router\NoRouteHandlerInterface
+{
+
+  public function process(\Magento\Framework\App\RequestInterface $request)
+  {
+      $requestValue = ltrim($request->getPathInfo(), '/');
+      $request->setParam('q', $requestValue);
+      $request->setModuleName('catalogsearch')->setControllerName('result')->setActionName('index');
+      return true;
+  }
+}
---
+++ Controller/Router.php
@@ -0,0 +1,54 @@
+<?php
+namespace Monish\HelloWorld\Controller;
+
+/**
+ * Aureatelabs Custom router Controller Router
+ */
+class Router implements \Magento\Framework\App\RouterInterface
+{
+    /**
+     * @var \Magento\Framework\App\ActionFactory
+     */
+    protected $actionFactory;
+
+    /**
+     * Response
+     *
+     * @var \Magento\Framework\App\ResponseInterface
+     */
+    protected $_response;
+
+    /**
+     * @param \Magento\Framework\App\ActionFactory $actionFactory
+     * @param \Magento\Framework\App\ResponseInterface $response
+     */
+    public function __construct(\Magento\Framework\App\ActionFactory $actionFactory, \Magento\Framework\App\ResponseInterface $response)
+    {
+        $this->actionFactory = $actionFactory;
+        $this->_response = $response;
+    }
+
+    /**
+     * @param \Magento\Framework\App\RequestInterface $request
+     * @return \Magento\Framework\App\ActionInterface|void
+     */
+    public function match(\Magento\Framework\App\RequestInterface $request)
+    {
+        $identifier = trim($request->getPathInfo() , '/');        
+        if (strpos($identifier, 'monishhelloworld') !== false)
+        {
+           
+            $request->setModuleName('helloworld')
+                ->setControllerName('index')
+                ->setActionName('index');
+        }
+        else
+        {
+            return;
+        }
+
+        return $this
+            ->actionFactory
+            ->create('Magento\Framework\App\Action\Forward', ['request' => $request]);
+    }
+}
